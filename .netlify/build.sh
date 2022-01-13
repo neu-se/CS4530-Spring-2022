e09395fa2044ec7c27d2243da44c8a3c382d020f
Jonathan Bell <jon@jonbell.net>
2022-01-13 13:50:32 -0500

    :facepalm: eventually this will be right everywhere

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -2,4 +2,4 @@
 ESCAPED_URL=$(printf '%s\n' "$DEPLOY_URL" | sed -e 's/[\/&]/\\&/g')
 
 sed -i -e "s/^url:.*$/url: '$ESCAPED_URL'/g" _config.yml
-bundle exec jekyll build -b /
+bundle exec jekyll build -b ''
